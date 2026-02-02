35ea762206cfe74b156b40b4f4120e4b8826aa24
Syr873 <mtroi873@gmail.com>
2026-02-02 19:04:20 -0300

    Numpy e Pandas aplicados em dados reais da web

--- atividade_estatistica_estel.py
+++ atividade_estatistica_estel.py
@@ -29,5 +29,86 @@ try:
     print(f'Mediana: {mediana}')
     print(f'Distância Média e Mediana: {distancia_media_mediana:.3f}')
 
+
+
 except Exception as e:
     print(f'Erro ao obter informações... {e}')
+
+
+ # Obtendo medidas estatísticas
+try:
+
+    q1 = np.quantile(array_estelionato, .25)
+    q2 = np.quantile(array_estelionato, .50)
+    q3 = np.quantile(array_estelionato, .75)
+
+    print('\nMedidas de Posição:')
+    print(30*'=')
+    print(f'Q1: {q1}')
+    print(f'Q2: {q2}')
+    print(f'Q3: {q3}')
+
+    # menores
+    df_estelionato_menores = df_estelionato_ordenado[df_estelionato_ordenado['estelionato'] < q1]
+    # maiores
+    df_estelionato_maiores = df_estelionato_ordenado[df_estelionato_ordenado['estelionato'] > q3]
+
+    print('\nMenores')
+    print(30*'=')
+    print(df_estelionato_menores.sort_values(by='estelionato', ascending=True))
+    
+    print('\nMaiores')
+    print(30*'=')
+    print(df_estelionato_maiores.sort_values(by='estelionato', ascending=False))
+
+    # Mdidas de Dispersão
+    maximo = np.max(array_estelionato)
+    minimo = np.min(array_estelionato)
+    amplitude_total = maximo - minimo
+
+    print('\nPrintando Medidas de Dispersão:')
+    print(30*'=')
+    print(f'Máximo: {maximo}')
+    print(f'Mínimo: {minimo}')
+    print(f'Amplitude Total: {amplitude_total}')
+
+
+    # Intervalo interquartil
+    iqr = q3 - q1
+    
+
+    # Limite Inferior
+    limite_inferior = q1 - (1.5 * iqr)
+
+    # Limite superior
+    limite_superior = q3 + (1.5 * iqr)
+
+    print('\nMedidas de Dispersão')
+    print(30*'=')
+    print(f'IQR: {iqr}')
+    print(f'Limite Inferior {limite_inferior}')
+    print(f'Limite Superior {limite_superior}')
+
+
+    # Outliers Inferiores:
+    df_estelionato_outliers_inferiores = df_estelionato_ordenado[df_estelionato_ordenado['estelionato'] < limite_inferior]
+
+    # Outliers superiores:
+    df_estelionato_outliers_superiores = df_estelionato_ordenado[df_estelionato_ordenado['estelionato'] > limite_superior]
+
+    print('\nOutliers Inferiores:')
+    if len(df_estelionato_outliers_inferiores) == 0:
+        print('Não há outliers inferiores')
+    else:
+        print(df_estelionato_outliers_inferiores.sort_values(by='estelionato', ascending=True))
+
+
+    print('\nOutliers Superiores:')
+    if len(df_estelionato_outliers_superiores) == 0:
+        print('Não há outliers superiores')
+    else:
+        print(df_estelionato_outliers_superiores.sort_values(by='estelionato', ascending=False))
+    
+
+except Exception as e:
+    print(f'Erro ao obter medidas estatísticas {e}')
